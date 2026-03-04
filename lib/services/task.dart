72686f8348671678c152346408dd0dfaa5dda0e6
Abdullah <abdullahrehman1019@gmail.com>
2026-03-04 11:58:23 -0600

    models and services

---
+++ lib/services/task.dart
@@ -0,0 +1,12 @@
+class TaskServices{
+
+  ///Create Task
+  ///Get All Task
+  ///Update Task
+  ///Delete Task
+  ///Completed Task
+  ///InCompleted Task
+  ///Mark As Completed
+  ///Filter Task
+  ///Search Task
+}
